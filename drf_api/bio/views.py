001222e01449ab7f4c5ea96e6f00ba4dc23a65bc
allan zizinga <allanzizinga@yahoo.com>
2024-09-11 05:58:55 +0200

    Create update function

--- drf_api/bio/views.py
+++ drf_api/bio/views.py
@@ -35,6 +35,33 @@ class BioDetail(APIView):
         bio = self.get_object()
         serializer = BioSerializer(bio, context={'request': request})
         return Response(serializer.data,  status=status.HTTP_200_OK)
+    
+    def get_object(self):
+        try:
+            return Bio.objects.get(pk=self.kwargs['pk'])
+        except Bio.DoesNotExist:
+            raise NotFound('Bio does not exist...')
+        
+    def put(self, request, pk):
+        """
+        This function will update a profile,nOT WORKING RIGHT NOW.
+        """
+        try:
+            Bio.objects.get(pk=self.kwargs['pk'])
+        except Bio.DoesNotExist:
+            raise NotFound('Bio does not exist!!!')
+
+
+        bio =self.get_object()
+        serializer = BioSerializer(bio, data=request.data, context={'request':request})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
     
