812608a13b23edcd3f2f9a85ededf1b4790270f6
allan zizinga <allanzizinga@yahoo.com>
2024-09-11 05:20:36 +0200

    Create function to retrieve profile by ID

--- drf_api/bio/urls.py
+++ drf_api/bio/urls.py
@@ -3,4 +3,5 @@ from bio  import views
 
 urlpatterns = [
     path('bios/',views.BioList.as_view()),
+    path('bios/<int:pk>',views.BioDetail.as_view()),
 ]
--- drf_api/bio/views.py
+++ drf_api/bio/views.py
@@ -1,15 +1,18 @@
-
+from django.http import Http404
 from .models import Bio
 from rest_framework.views  import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework import status
 from .serializers import BioSerializer
+# from rest_framework.decorators import api_view, permission_classes
 
 
 # Create your views here.
 
 class BioList(APIView):
     """
-    List all bios
+    List all profiles created in the database.
     """
     def get(self,request):
         bio = Bio.objects.all()
@@ -17,5 +20,22 @@ class BioList(APIView):
         return Response(serializer.data)
 
     
+class BioDetail(APIView):
+    """
+    This function will return one profile based on the ID.
+    """
+    def get_object(self):
+        try:
+            return Bio.objects.get(pk=self.kwargs['pk'])
+        except Bio.DoesNotExist:
+            raise NotFound('Bio does not exist')
 
+
+    def get(self,request, pk):
+        bio = self.get_object()
+        serializer = BioSerializer(bio, context={'request': request})
+        return Response(serializer.data,  status=status.HTTP_200_OK)
+
+
+    
 
